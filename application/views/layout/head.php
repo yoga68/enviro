e3a4c47dcdd9430dd9206354e00ba4f8b11a3787
yoga68 <yogapriatama738@gmail.com>
2019-10-17 14:23:30 +0700

    title

--- application/views/layout/head.php
+++ application/views/layout/head.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>Enviro Buana Cipta</title>
+  <title>Ena Cipta</title>
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <meta content="Enviro, EBC, GFF" name="keywords">
   <meta content="" name="description">
